80f72f5cda2805271838498e9b8c92a9defd9e5b
Jean Lima <jean.jr.souza@hotmail.com>
2020-08-13 15:15:15 -0300

    Metodos para update e delete de cliente.

--- src/main/java/br/com/jean/VendasApplication.java
+++ src/main/java/br/com/jean/VendasApplication.java
@@ -3,13 +3,10 @@ package br.com.jean;
 import br.com.jean.domain.entity.Cliente;
 import br.com.jean.domain.repository.ClienteReposiory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
@@ -19,7 +16,7 @@ import java.util.List;
 public class VendasApplication {
 
     @Bean
-    public CommandLineRunner init(@Autowired ClienteReposiory clienteReposiory){
+    public CommandLineRunner init(@Autowired ClienteReposiory clienteReposiory) {
         return args -> {
             Cliente cliente = new Cliente();
             cliente.setNome("Jean");
@@ -31,7 +28,11 @@ public class VendasApplication {
             clienteReposiory.save(c2);
 
             List<Cliente> todos = clienteReposiory.getAll();
-          todos.forEach(System.out::println);
+            todos.forEach(System.out::println);
+
+            clienteReposiory.delete(c2.getId());
+        clienteReposiory.delete(1);
+           clienteReposiory.getByName("Jean");
         };
     }
 
--- src/main/java/br/com/jean/domain/repository/ClienteReposiory.java
+++ src/main/java/br/com/jean/domain/repository/ClienteReposiory.java
@@ -12,29 +12,62 @@ import java.sql.SQLException;
 import java.util.List;
 
 @Repository
-public class ClienteReposiory  {
+public class ClienteReposiory {
 
-    private static  String INSERT ="insert into cliente (nome) values (?)";
+    private static String INSERT = "insert into cliente (nome) values (?)";
     private static String SELECT_ALL = "SELECT * FROM cliente";
+    private static String UPDATE = "update cliente set nome = ? where id =?";
+    private static String DELETE = "delete from cliente where id = ?";
+
 
 
     @Autowired
     private JdbcTemplate jdbcTemplate;
 
-    public Cliente save(Cliente cliente){
-    jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()}  );
-    return cliente;
+    public Cliente save(Cliente cliente) {
+        jdbcTemplate.update(INSERT, new Object[]{cliente.getNome()});
+        return cliente;
     }
 
-    public List<Cliente> getAll(){
-        return jdbcTemplate.query(SELECT_ALL, new RowMapper<Cliente>() {
+    public List<Cliente> getAll() {
+        return jdbcTemplate.query(SELECT_ALL, getRowMapper());
+    }
+
+
+
+    public Cliente atualizar(Cliente cliente) {
+        jdbcTemplate.update(UPDATE, new Object[]{cliente.getNome(),
+                cliente.getId()});
+
+        return cliente;
+    }
+
+
+    public List<Cliente> getByName(String nome){
+    return jdbcTemplate
+            .query(SELECT_ALL.concat(" where nome like ?"),
+                    new Object[]{"%" + nome + "%"},
+                    getRowMapper());
+    }
+
+    public void delete(Cliente cliente) {
+        delete(cliente.getId());
+    }
+
+    public void delete(Integer id) {
+        jdbcTemplate.update(DELETE, new Object[]{id});
+    }
+
+
+    private RowMapper<Cliente> getRowMapper() {
+        return new RowMapper<Cliente>() {
             @Override
             public Cliente mapRow(ResultSet resultSet, int i) throws SQLException {
                 Integer id = resultSet.getInt("id");
                 String nome = resultSet.getString("nome");
-                return new Cliente(id,nome);
+                return new Cliente(id, nome);
             }
-        });
+        };
     }
 
 }
